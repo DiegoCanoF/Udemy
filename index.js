fdc6b5b878e5bcafb98652f9a5a3dac2c447be11
DiegoCanoF <ca477532@uaeh.edu.mx>
2025-05-14 01:34:14 -0600

    Primer commit

--- index.js
+++ index.js
@@ -4,36 +4,34 @@ const cors = require('cors');
 const bodyParser = require('body-parser');
 const path = require('path');
 
+// Configuración de middlewares
+app.use(cors());
+app.use(bodyParser.json());
 
-
-app.use(express.static(path.join(__dirname, 'public')));
-
-// Importar rutas
+// Rutas API
 const authRoutes = require('./routes/auth');
-const cursosRoutes = require('./routes/cursos');  // Asegúrate de que la ruta sea correcta
+const cursosRoutes = require('./routes/cursos');
 const inscripcionesRoutes = require('./routes/inscripciones');
 const videosRoutes = require('./routes/videos');
 const formulariosRoutes = require('./routes/formularios');
 const imagenesRoutes = require('./routes/imagenes');
 const archivosRoutes = require('./routes/archivos');
 
-
-
-// Configuración de middlewares
-app.use(cors());
-app.use(bodyParser.json());
-
-// Definir rutas
 app.use('/api/auth', authRoutes);
-app.use('/api/cursos', cursosRoutes);  // Las rutas de cursos están bajo /api/cursos
+app.use('/api/cursos', cursosRoutes);
 app.use('/api/inscripciones', inscripcionesRoutes);
 app.use('/api/videos', videosRoutes);
 app.use('/api/formularios', formulariosRoutes);
 app.use('/api/imagenes', imagenesRoutes);
 app.use('/api/archivos', archivosRoutes);
 
+// Servir archivos estáticos
+app.use(express.static(path.join(__dirname, 'public')));
 
-
+// Redirigir raíz al login
+app.get('/', (req, res) => {
+  res.redirect('/login.html');
+});
 
 // Iniciar servidor
 const port = 3000;
